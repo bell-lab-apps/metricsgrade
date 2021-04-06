6f24590e92fbff191c2a1500537f6301244d878b
Warren Galyen <wgalyen@bellhelmets.com>
2021-04-06 09:15:30 -0700

    feat(waitUntil): add possibiliy to add waitUntil option to page.reload

--- cli.js
+++ cli.js
@@ -21,6 +21,7 @@ program
   .option('-H, --height [height]', "The viewport's height to set", DEFAULT_VIEWPORT_SIZE.HEIGHT)
   .option('-c, --custom-path [custom-path]', 'Path to custom path configuration file')
   .option('-o, --output-format [output-format]', 'The desired output format', DEFAULT_OUTPUT_FORMAT.CLI)
+  .option('--wait-until [wait-until]', 'The waitUntil value of the Page.reload options accepted by puppeteer')
   .option('--output-file [output-file]', 'Whether we want to export data in a file, and the desired path to the file')
   .option('--no-headless', 'Defines if we dont want to use puppeteer headless mode')
   .parse(process.argv);
